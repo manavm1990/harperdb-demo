099638cb6d902360e86dec566b0d0c398ce15096
Manav Misra <manavm@visionwebsoft.com>
2020-09-16 08:40:05 -0500

    :bug: Prevent 'flicker' on every update

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import { useHarperDB } from "use-harperdb"
 
@@ -7,7 +7,13 @@ import { StarRatings } from "./StarRatings"
 
 import "./Reviews.css"
 
+function compareByStringify(a, b) {
+  return JSON.stringify(a) === JSON.stringify(b)
+}
+
 export const Reviews = ({ type = "books" }) => {
+  const [reviews, setReviews] = useState([])
+
   const [data, loading, error] = useHarperDB({
     query: {
       operation: "sql",
@@ -16,34 +22,38 @@ export const Reviews = ({ type = "books" }) => {
     interval: process.env.REACT_APP_INTERVAL,
   })
 
-  if (loading) {
+  useEffect(() => {
+    if (!compareByStringify(data, reviews)) {
+      setReviews(data)
+    }
+  }, [data, reviews])
+
+  if (!compareByStringify(data, reviews) && loading) {
     return <div>Loading...</div>
   }
-  if (data?.length) {
+  if (reviews?.length) {
     return (
-      <>
-        <table>
-          <thead>
-            <tr>
-              <td>Book</td>
-              <td>Author</td>
-              <td>Rating</td>
+      <table>
+        <thead>
+          <tr>
+            <td>Book</td>
+            <td>Author</td>
+            <td>Rating</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          {reviews.map(({ bookid, name, author, review }) => (
+            <tr key={bookid}>
+              <td>{name}</td>
+              <td>{author}</td>
+              <td>
+                <StarRatings rating={review} />
+              </td>
             </tr>
-          </thead>
-          
-          <tbody>
-            {data.map(({ bookid, name, author, review }) => (
-              <tr key={bookid}>
-                <td>{name}</td>
-                <td>{author}</td>
-                <td>
-                  <StarRatings rating={review} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </>
+          ))}
+        </tbody>
+      </table>
     )
   }
   return <p>No data! {error}</p>
